81dbdbfc200b8f5fba76f8de63ba20e079eeb5c8
jambalaja9 <raphael.hafner@hotmail.de>
2015-10-15 10:45:43 +0200

    login und divs
    login functioniert und die divs zum navigieren sind da

--- nav.js
+++ nav.js
@@ -1,17 +1,38 @@
-function navigate(value) {
-    
-    switch (value);
-        case "login": 
-            document.location.pathname = "login";
-            break;
-        case "home":
-            document.location.pathname = "calender";
-            break;
-        case "calender":
-            document.location.pathname = "home";
-            break;
-        case "picture":
-            document.location.pathname = "picture";
-        default:
-            document.location.pathname = "login";
-    
+function nav(){
+                   
+    if(window.location.href.indexOf("index") > -1) {
+        document.getElementById("login").style.display = "block";
+        document.getElementById("calender").style.display = "none";
+        document.getElementById("home").style.display = "none";
+        document.getElementById("picture").style.display = "none";
+    }
+    else if(window.location.href.indexOf("calender") > -1) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("calender").style.display = "block";
+        document.getElementById("home").style.display = "none";
+        document.getElementById("picture").style.display = "none";
+    }
+    else if(window.location.href.indexOf("home") > -1) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("calender").style.display = "none";
+        document.getElementById("home").style.display = "block";
+        document.getElementById("picture").style.display = "none";
+    }
+    else if(window.location.href.indexOf("picture") > -1) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("calender").style.display = "none";
+        document.getElementById("home").style.display = "none";
+        document.getElementById("picture").style.display = "block";
+    }
+        
+        
+        
+        
+        
+        
+        
+            document.getElementById("kalender").visibility =  document.location.endsWith("kalender.html");
+            
+            
+        
+            
