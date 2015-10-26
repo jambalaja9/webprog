a3802c72774360d079d27fb5380aa5a01993f307
jambalaja9 <raphael.hafner@hotmail.de>
2015-10-26 13:04:34 +0100

    navigation
    funktioniert

--- nav.js
+++ nav.js
@@ -1,17 +1,18 @@
-function back() {
-                   
-    if(window.location.href.indexOf("calendar") > -1) {
-        
-        history.pushState(null,"","home.html");
-        
+window.addEventListener("popstate",navigate);
+
+
+
+function navigate() {
+          
+    alert("back");
+    if(window.location.href.indexOf("home") > -1) {
+    
         document.getElementById("login").style.display = "none";
         document.getElementById("calendar").style.display = "none";
         document.getElementById("home").style.display = "block";
         document.getElementById("file").style.display = "none";
     }
-    else if(window.location.href.indexOf("home") > -1) {
-        
-        history.pushState(null,"","index.html");
+    else if(window.location.href.indexOf("index") > -1) {
         
         document.getElementById("login").style.display = "block";
         document.getElementById("calendar").style.display = "none";
@@ -20,14 +21,21 @@ function back() {
     }
     else if(window.location.href.indexOf("file") > -1) {
         
-        history.pushState(null,"","index.html");
-        
         document.getElementById("login").style.display = "none";
         document.getElementById("calendar").style.display = "none";
         document.getElementById("home").style.display = "block";
         document.getElementById("file").style.display = "none";
     }
+    else if(window.location.href.indexOf("calendar") > -1) {
+        
+        document.getElementById("login").style.display = "none";
+        document.getElementById("calendar").style.display = "block";
+        document.getElementById("home").style.display = "none";
+        document.getElementById("file").style.display = "none";
+    }
 }
+
+
         
 
             
